5f15918d55c7bbc1685004300c365a51426bcdaf
awskh013 <awskh013@gmail.com>
2025-11-26 09:41:56 +0300

    Add files via upload

---
+++ ttss.py
@@ -0,0 +1,29 @@
+from flask import Flask, send_file, jsonify
+import asyncio
+import edge_tts
+import os
+
+app = Flask(__name__)
+OUTPUT_FILE = "output.mp3"
+
+async def text_to_speech(text, voice="en-US-JennyNeural"):
+    communicate = edge_tts.Communicate(text, voice)
+    await communicate.save(OUTPUT_FILE)
+
+@app.route("/tts", methods=["GET"])
+def tts():
+    text = "Hi Haaamooodi"
+    voice = "en-US-JennyNeural"
+
+    try:
+        asyncio.run(text_to_speech(text, voice))
+        return send_file(OUTPUT_FILE, mimetype="audio/mpeg", as_attachment=True)
+
+    except Exception as e:
+        print("🔥 ERROR OCCURRED:", e)   # يطبع الخطأ مهما كان
+        import traceback
+        traceback.print_exc()           # يطبع Stacktrace كامل
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False)
